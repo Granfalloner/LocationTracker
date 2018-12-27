f0c31b4c896ad2a7085bb0ad66b95f206e1e650e
Vasyl Myronchuk <granfalloner@gmail.com>
2018-12-28 00:38:38 +0100

    [LTKit] Added LKKit framework containing base shared functionality.

---
+++ LTKit/Kit/Sources/LTKit.h
@@ -0,0 +1,19 @@
+//
+//  LTKit.h
+//  LTKit
+//
+//  Created by Vasyl Myronchuk on 27/12/2018.
+//  Copyright © 2018 Vasyl Myronchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LTKit.
+FOUNDATION_EXPORT double LTKitVersionNumber;
+
+//! Project version string for LTKit.
+FOUNDATION_EXPORT const unsigned char LTKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LTKit/PublicHeader.h>
+
+
